34984c7f96e90226cb074c75bb9b962e268371cb
reo0306 <reo19840306@gmail.com>
2025-08-20 15:56:49 +0900

    add v-if

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -107,9 +107,18 @@ function toggleClass() {
   isRed.value = !isRed.value;
   isBlue.value = !isBlue.value;
 }
+
+const ok = ref(true);
+const maybeOk = ref(false);
 </script>
 
 <template>
+  <!-- v-if, v-else-if v-else -->
+  <button @click="ok = !ok">toggle</button>
+  <p v-if="ok">OK!</p>
+  <p v-else-if="maybeOk">maybe OK!</p>
+  <p v-else>not OK...</p>
+
   <div>{{ count + count2 }}</div>
   <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
   <div>{{ count }}</div>
