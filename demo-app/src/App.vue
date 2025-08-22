02ae92208eced3424028a3b28bad319a57a54171
reo0306 <reo19840306@gmail.com>
2025-08-23 06:51:06 +0900

    add food project

---
+++ demo-app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>今日何食べる？</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
+</style>
---
+++ demo-app/src/components/FoodCard.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>寿司</div>
+  <div></div>
+</template>
