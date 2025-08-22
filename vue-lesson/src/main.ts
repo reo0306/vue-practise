a8d8f612dfec5ce5d3df3a4f2916fd806e1910b5
reo0306 <reo19840306@gmail.com>
2025-08-22 13:52:56 +0900

    add vite css

--- vue-lesson/src/main.ts
+++ vue-lesson/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import BaseIcon from '@/components/BaseIcon.vue'
+import './assets/main.css'
 
 createApp(App).component('BaseIcon', BaseIcon).mount('#app')
