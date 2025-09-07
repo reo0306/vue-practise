8843a5fa47be0962a8387e48836ed4df1720d58d
reo0306 <reo19840306@gmail.com>
2025-09-07 15:13:12 +0900

    add router link

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -229,6 +229,8 @@ const newFruit = ref('');
 
 <template>
   <h1>Vue Router</h1>
+  <RouterLink to="/">Home</RouterLink> |
+  <RouterLink to="/about">About</RouterLink>
   <RouterView />
 
   <p>{{ countCon }}</p>
