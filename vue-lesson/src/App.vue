3bd54fecac90f2a9179e668ae5cbd413592dc44d
reo0306 <reo19840306@gmail.com>
2025-09-07 15:17:43 +0900

    update router link

--- vue-lesson/src/App.vue
+++ vue-lesson/src/App.vue
@@ -229,8 +229,8 @@ const newFruit = ref('');
 
 <template>
   <h1>Vue Router</h1>
-  <RouterLink to="/">Home</RouterLink> |
-  <RouterLink to="/about">About</RouterLink>
+  <RouterLink :to="'/?lang=ja#title'">Home</RouterLink> |
+  <RouterLink :to="{path: '/about', query: { lang: 'ja' }, hash: '#title'}">About</RouterLink>
   <RouterView />
 
   <p>{{ countCon }}</p>
