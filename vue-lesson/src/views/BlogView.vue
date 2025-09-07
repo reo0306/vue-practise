07527d8bbb6ad3d5bb7d48f5d5a54e73f2b71ba7
reo0306 <reo19840306@gmail.com>
2025-09-07 15:42:11 +0900

    update BlogView

--- vue-lesson/src/views/BlogView.vue
+++ vue-lesson/src/views/BlogView.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, watch } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+console.log(route.params.id);
+watch(
+  () => route.params.id,
+  () => {
+    console.log('watch')
+  }
+)
+onMounted(() => {
+  console.log('onMounted');
+})
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+</script>
+
 <template>
   <h2>Blog</h2>
+  <p>Blog(id: {{ $route.params.id }})</p>
+  <p>ver: {{ $route.params.version }}</p>
+  <RouterLink :to="{ name: 'blog', params: { id: Number($route.params.id) + 1, version: $route.params.version }}">Next</RouterLink>
 </template>
