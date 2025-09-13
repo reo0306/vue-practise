b71a9df24124dbe7525d2f67ca040aa07060723a
reo0306 <reo19840306@gmail.com>
2025-09-14 06:22:31 +0900

    update meta

--- vue-lesson/src/views/HomeView.vue
+++ vue-lesson/src/views/HomeView.vue
@@ -3,6 +3,7 @@
 
 <template>
   <h2>Home</h2>
+  <p>{{ $route.meta }}</p>
   <h3>Blog</h3>
   <RouterLink :to="{ name: 'blog', params: { id: 3, version: 'foo' } }">New Release!</RouterLink> |
   <RouterLink :to="{ name: 'profile', params: { id: 'yoshipi' } }">Profile</RouterLink>
