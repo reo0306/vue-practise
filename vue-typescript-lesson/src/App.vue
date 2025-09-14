9c6c9d16fc713bfe8f79cf5dff8df7002cdec6a4
reo0306 <reo19840306@gmail.com>
2025-09-15 06:41:16 +0900

    add useTemplaeRef

--- vue-typescript-lesson/src/App.vue
+++ vue-typescript-lesson/src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref, useTemplateRef } from 'vue';
 
 const count = ref(0)
 const dobuleCount = computed<number>(() => {
   if (count.value > 3) return 100
   return count.value * 2
 })
-dobuleCount.value
+
+const userInput = useTemplateRef<HTMLElement>('user-input')
+onMounted(() => {
+  userInput.value?.focus()
+})
 </script>
 
 <template>
   <h1>TypeScript</h1>
+  <input ref="user-input" type="text" />
 </template>
